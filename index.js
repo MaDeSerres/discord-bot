b56fbc6ebb2ced2133fa0104294794f4c35e482b
Marc-Antoine De Serres <marc-antoine.de-serres@polymtl.ca>
2020-02-07 22:55:03 -0500

    Fixed an unknown error

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 const fs = require('fs');
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
+// create a new Discord client
 const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
 }
 
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
 client.once('ready', () => {
-  console.log('Ready!');
+	console.log('Ready!');
 });
 
 client.on('message', (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).split(/ +/)
-  const cmd = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
 
-  if (!client.commands.has(command)) return;
+	if (!client.commands.has(command)) return;
 
-  try {
-    client.commands.get(command).execute(message, args);
-  } catch (err) {
-    console.error(err);
-    message.reply('There was an error trying to execute that command!');
-  }
-
+	try {
+		client.commands.get(command).execute(message, args);
+	}
+	catch (err) {
+		console.error(err);
+		message.reply('There was an error trying to execute that command!');
+	}
 });
 
+
+// login to Discord with your app's token
 client.login(token);
